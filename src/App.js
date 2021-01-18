164125b08f630cac24ab2da1f4548b4013b6794c
David Grant <davidgrant132@gmail.com>
2021-01-18 21:50:28 +0000

    buttons change on hover function

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState } from "react"
+import Button from "./components/Button"
 
 function App() {
+
+  const [color, setColor] = useState(`blue`);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div
+      className={`App`}
+      style={{
+        backgroundColor: color,
+      }}
+    >
+      <div className="container">
+        <h1>Hello World!</h1>
+        <Button color="red" setColor={setColor}/>
+        <Button color="yellow" setColor={setColor}/>
+        <Button color="blue" setColor={setColor}/>
+        <Button color="purple" setColor={setColor}/>
+        <Button color="tomato" setColor={setColor}/>
+      </div>
     </div>
   );
 }
---
+++ src/components/Button.js
@@ -0,0 +1,14 @@
+function Button({color, setColor}) {
+
+    return(
+        <button
+            // className={color}
+            style={{
+                backgroundColor: color,
+            }}
+            onMouseOver={() => {
+          setColor(color);
+        }}>{color}</button>
+        )
+}
+export default Button;
